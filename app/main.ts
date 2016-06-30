50f898d696645beb6f60a947c4433f5b27089a45
Daniel Donaldson <daniel.donaldson@turner.com>
2016-06-30 19:31:14 -0400

    starting level 2 7

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,38 @@
-import {bootstrap}    from '@angular/platform-browser-dynamic';
-import {AppComponent} from './app.component';
+import {bootstrap}	from '@angular/platform-browser-dynamic';
+import {Component}	from '@angular/core';
+
+
+@Component({
+	selector: 'my-app',
+	template: `<h1>{{title}}</h1>
+		<ul>
+			<li *ngFor="let carPart of carParts">
+				<h2>{{carPart.name}}</h2>
+				<p>{{carPart.description}}</p>
+				<p *ngIf="carPart.inStock > 0">{{carPart.inStock}}	in Stock</p>
+				<p *ngIf="carPart.inStock === 0">Out of Stock</p>
+			</li>
+		</ul>`
+})
+
+class AppComponent{ 
+	title = 'Ultra Racing';
+	carParts = [{
+		"id" : 1,
+		"name" : "Super Tires",
+		"description" : "These tires are the very best",
+		"inStock" : 5
+	},
+	{
+		"id" : 2,
+		"name" : "Reinforced Shocks",
+		"inStock" : 4
+	},
+	{
+		"id" : 3,
+		"name" : "Turbocharger",
+		"inStock" : 0
+	}];									
+}
 
 bootstrap(AppComponent);

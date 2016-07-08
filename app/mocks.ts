547c3f5232e4c9637db76cfeac15f6ab17d568e3
Daniel Donaldson <daniel.donaldson@turner.com>
2016-07-08 15:58:17 -0400

    forgot to add files to last commit

---
+++ app/mocks.ts
@@ -0,0 +1,21 @@
+import { CarPart } from './car-part.ts';
+
+export const CARPARTS: CarPart[] = [{
+	"id" : 1,
+	"name" : "Super Tires",
+	"description" : "These tires are the very best",
+	"inStock" : 5,
+	"price" : 39.99
+},
+{
+	"id" : 2,
+	"name" : "Reinforced Shocks",
+	"inStock" : 4,
+	"price" : 29.99
+},
+{
+	"id" : 3,
+	"name" : "Turbocharger",
+	"inStock" : 0,
+	"price" : 199.99
+}];
